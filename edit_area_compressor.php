f34d8034cc3e1edbdf68e4bee51843832c12cd5b
Ger Hobbelt <ger@hobbelt.com>
2011-01-17 01:13:19 +0100

    typo fix

--- edit_area_compressor.php
+++ edit_area_compressor.php
@@ -11,7 +11,7 @@
 	// CONFIG
 	$param['cache_duration']= 3600 * 24 * 10;		// 10 days util client cache expires
 	$param['compress'] = true;						// Enable the code compression, should be activated but it can be useful to deactivate it for easier error diagnostics (true or false)
-	$param['debug'] = false;						// Enable this option if you need debuging info
+	$param['debug'] = false;						// Enable this option if you need debugging info
 	$param['use_disk_cache'] = true;				// If you enable this option gzip files will be cached on disk.
 	$param['use_gzip']= true;						// Enable gzip compression
 	// END CONFIG
